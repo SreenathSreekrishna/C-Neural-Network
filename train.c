49f9d3270e91b6f18a6434e8b42d872e36b659ba
Sreenath Sreekrishna <sreenathsreekrishna21@gmail.com>
2022-05-31 17:15:10 +0530

    fix compute function

--- train.c
+++ train.c
@@ -2,6 +2,7 @@
 
 Vector compute(Vector input, Network nn){
     //Computes output neuron activations given the input neuron activations
+    nn.neuronLayers[0].neurons = input;
     for (int i = 0; i<nn.size-1; i++){
         for (int j = 0; j<nn.neuronLayers[i+1].neurons.length; j++){
             float result = nn.neuronLayers[i+1].biases.arr[j] + vDot(nn.neuronLayers[i].neurons, nn.weightLayers[i].weights.arr[j]);
